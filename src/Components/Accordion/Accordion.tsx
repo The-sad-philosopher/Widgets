cbe52501249cc517dd858ef9e65fe1cc3bfc4ef9
The-sad-philosopher <shinudonney@tuta.io>
2021-01-02 21:29:23 +0530

    Add a basic accordion with some styles 🗼

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -7,5 +7,26 @@ type Props = {
 };
 
 export const Accordion: React.FC<Props> = ({ items }: Props) => {
-  return <div className="accordion"> {items.length} </div>;
+  const [activeIndex, setActiveIndex] = React.useState<number | null>(null);
+
+  const onTitleClick = (index: number) => setActiveIndex(index);
+
+  const list = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div
+          className={`title ${index === activeIndex ? 'active' : ''}`}
+          onClick={() => onTitleClick(index)}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${index === activeIndex ? 'active' : ''}`}>
+          {item.content}
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{list}</div>;
 };
