f76d376caefdbeecc83883863836570f7fd82979
The-sad-philosopher <shinudonney@tuta.io>
2021-01-04 08:11:47 +0530

    Make react controlled input element  ⛵

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
+import { WikiSearch } from '../WikiSearch';
 import './App.sass';
-import { Accordion } from '../Accordion';
-import { items } from '../../helpers';
 
 export const App: React.FC = () => {
   return (
     <div className="app">
-      <Accordion items={items} />
+      <WikiSearch />
     </div>
   );
 };
--- src/Components/WikiSearch/WikiSearch.tsx
+++ src/Components/WikiSearch/WikiSearch.tsx
@@ -1,5 +1,25 @@
 import * as React from 'react';
 
 export const WikiSearch: React.FC = () => {
-  return <div>WikiSearch</div>;
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setSearchTerm(e.target.value);
+
+  return (
+    <div className="wiki-search">
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="search">Enter search term</label>
+          <input
+            type="text"
+            placeholder="Search"
+            className="input"
+            onChange={onInputChange}
+            value={searchTerm}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
