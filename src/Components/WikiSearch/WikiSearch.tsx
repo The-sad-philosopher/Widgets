aab643f6141c259c221af06535f178411a46c88f
The-sad-philosopher <shinudonney@tuta.io>
2021-01-04 08:14:14 +0530

    Small refactoring  ⚙

--- src/Components/WikiSearch/WikiSearch.tsx
+++ src/Components/WikiSearch/WikiSearch.tsx
@@ -7,18 +7,16 @@ export const WikiSearch: React.FC = () => {
     setSearchTerm(e.target.value);
 
   return (
-    <div className="wiki-search">
-      <div className="ui form">
-        <div className="field">
-          <label htmlFor="search">Enter search term</label>
-          <input
-            type="text"
-            placeholder="Search"
-            className="input"
-            onChange={onInputChange}
-            value={searchTerm}
-          />
-        </div>
+    <div className="wiki-search ui form">
+      <div className="field">
+        <label htmlFor="search">Enter search term</label>
+        <input
+          type="text"
+          placeholder="Search"
+          className="input"
+          onChange={onInputChange}
+          value={searchTerm}
+        />
       </div>
     </div>
   );
