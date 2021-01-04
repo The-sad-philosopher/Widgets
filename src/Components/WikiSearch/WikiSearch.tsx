357a520202ea2fa0f99405952f29a8f34e21afeb
The-sad-philosopher <shinudonney@tuta.io>
2021-01-04 20:38:03 +0530

    Add useEffect Hook  🎈

--- src/Components/WikiSearch/WikiSearch.tsx
+++ src/Components/WikiSearch/WikiSearch.tsx
@@ -3,6 +3,10 @@ import * as React from 'react';
 export const WikiSearch: React.FC = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
+  React.useEffect(() => {
+    // ? api call
+  }, [searchTerm]);
+
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setSearchTerm(e.target.value);
 
