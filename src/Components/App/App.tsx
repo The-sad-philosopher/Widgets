842138f2eff082ecdf7e82415fe368c911da44e9
The-sad-philosopher <shinudonney@tuta.io>
2021-01-10 01:54:26 +0530

    Remove WikiSearch Component from App till finalization 📌

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
-import { WikiSearch } from '../WikiSearch';
 import './App.sass';
 
 export const App: React.FC = () => {
-  return (
-    <div className="app">
-      <WikiSearch />
-    </div>
-  );
+  return <div className="app">Hello World</div>;
 };
