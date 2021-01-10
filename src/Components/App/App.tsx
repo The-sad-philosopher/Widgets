8de49521a663ff33d70029d9e797f9b7a33c1268
The-sad-philosopher <shinudonney@tuta.io>
2021-01-10 15:01:39 +0530

    Render Dropdown component in app 🥗

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import './App.sass';
+import { Dropdown } from '../Dropdown';
 
 export const App: React.FC = () => {
-  return <div className="app">Hello World</div>;
+  return (
+    <div className="app">
+      <Dropdown />
+    </div>
+  );
 };
