de4ed1f5aad9bb148c76d8dba15cdb97360b1ac3
The-sad-philosopher <shinudonney@tuta.io>
2021-01-15 22:25:28 +0530

    🔥 Remove Dropdown component temporarily to integrate with #main branch

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,18 +1,6 @@
 import * as React from 'react';
 import './App.sass';
-import { Dropdown } from '../Dropdown';
-import { options } from '../../helpers';
 
 export const App: React.FC = () => {
-  const [selection, setSelection] = React.useState(options[0]);
-
-  return (
-    <div className="app">
-      <Dropdown
-        selection={selection}
-        onSelectionChange={setSelection}
-        options={options}
-      />
-    </div>
-  );
+  return <div className="app"></div>;
 };
