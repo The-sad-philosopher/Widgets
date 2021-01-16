5293011d4c86e7dc0abd0ce19e8e52432c5a9cf9
The-sad-philosopher <shinudonney@tuta.io>
2021-01-16 12:52:24 +0530

    ✨ Add input form element for users to enter the text to be translated

--- src/Components/Translator/Translator.tsx
+++ src/Components/Translator/Translator.tsx
@@ -10,11 +10,24 @@ const options = [
 
 export const Translator: React.FC = () => {
   const [selectedLanguage, setSelectedLanguage] = React.useState(options[0]);
+  const [text, setText] = React.useState('');
 
-  console.log(selectedLanguage);
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
 
   return (
-    <div className="translator">
+    <div className="translator ui container">
+      <div className="ui form">
+        <div className="field">
+          <label htmlFor="user-text">Enter some text</label>
+          <input
+            type="text"
+            placeholder="Enter some text"
+            value={text}
+            onChange={onInputChange}
+          />
+        </div>
+      </div>
       <Dropdown
         options={options}
         selection={selectedLanguage}
