4e14a56786e68c7f904b17ff7d05d08af73bd831
The-sad-philosopher <shinudonney@tuta.io>
2021-01-16 21:26:43 +0530

    🔥 Temporarily remove Translator component from App

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import './App.sass';
-import { Translator } from '../Translator';
 
 export const App: React.FC = () => {
-  return (
-    <div className="app">
-      <Translator />
-    </div>
-  );
+  return <div className="app">Hello, World!</div>;
 };
