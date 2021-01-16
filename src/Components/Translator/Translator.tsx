5199d36fc1f4d31ce2bbbe18de1095ffd99c2fd2
The-sad-philosopher <shinudonney@tuta.io>
2021-01-16 11:41:06 +0530

    💄 Render the Translator component

--- src/Components/Translator/Translator.tsx
+++ src/Components/Translator/Translator.tsx
@@ -1,4 +1,23 @@
 import * as React from 'react';
 import './Translator.sass';
+import { Dropdown } from '../Dropdown';
 
-export const Translator: React.FC = () => <div>Translator!</div>;
+console.log(Dropdown);
+export const Translator: React.FC = () => {
+  const options = [
+    { label: 'Hindi', value: 'hi' },
+    { label: 'French', value: 'fr' },
+  ];
+
+  const [selectedLanguage, setSelectedLanguage] = React.useState(options[0]);
+
+  return (
+    <div>
+      <Dropdown
+        options={options}
+        selection={selectedLanguage}
+        onSelectionChange={setSelectedLanguage}
+      />
+    </div>
+  );
+};
