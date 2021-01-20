6d0a639bf4818b3081ab6f0c57fcfa223fbe5b68
The-sad-philosopher <shinudonney@tuta.io>
2021-01-20 23:12:36 +0530

    💄 Add some styles to let the user know which link is active

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,16 +2,31 @@ import * as React from 'react';
 import { Link } from './Link';
 
 export const Header: React.FC = () => {
+  const [currentPath, setCurrentPath] = React.useState(
+    window.location.pathname
+  );
+
+  React.useEffect(() => {
+    const onURLChange = () => setCurrentPath(window.location.pathname);
+
+    window.addEventListener('popstate', onURLChange);
+    return () => window.removeEventListener('popstate', onURLChange);
+  }, []);
+
+  const getClassName = (path: string) => {
+    return currentPath === path ? 'active item' : 'item';
+  };
+
   return (
     <div className="ui secondary pointing menu">
       <div className="ui container">
-        <Link href="/" className="item">
+        <Link href="/" className={getClassName('/')}>
           Accordion
         </Link>
-        <Link href="/search" className="item">
+        <Link href="/search" className={getClassName('/search')}>
           WikiSearch
         </Link>
-        <Link href="/translate" className="item">
+        <Link href="/translate" className={getClassName('/translate')}>
           Translate
         </Link>
       </div>
