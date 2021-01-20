4ba05fe79ac41cde0ba53a869acd682542c196c5
The-sad-philosopher <shinudonney@tuta.io>
2021-01-20 14:34:55 +0530

    🏗 Add a basic router (made from scratch)

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,6 +1,20 @@
 import * as React from 'react';
 import './App.sass';
+import { items } from '../../helpers';
+import { Accordion } from '../Accordion';
+import { WikiSearch } from '../WikiSearch';
+import { Translator } from '../Translator';
+
+const route = (path: string, component: JSX.Element) => {
+  return window.location.pathname === path ? component : null;
+};
 
 export const App: React.FC = () => {
-  return <div className="app">Hello, World!</div>;
+  return (
+    <div className="app">
+      {route('/', <Accordion items={items} />)}
+      {route('/search', <WikiSearch />)}
+      {route('/translate', <Translator />)}
+    </div>
+  );
 };
